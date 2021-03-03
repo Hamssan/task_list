5a2038be8a48905878d1df0ce30361447583be70
Hamssan <hassanaitmoussa6@gmail.com>
2021-03-03 01:25:40 +0100

    implemented the clear tasks btn & trash icon

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const form = document.querySelector("#task-form");
-const taskInput = document.querySelector("#task");
-const taskCollection = document.querySelector(".collection");
+const AddTaskInput = document.querySelector("#task");
+const taskList = document.querySelector(".collection");
+const clearTasksBtn = document.querySelector(".clear-tasks");
 
+// Add Task
 form.addEventListener("submit", (e) => {
-  AddTask();
   e.preventDefault();
-});
 
-function AddTask() {
-  if (taskInput.value === "") {
+  if (AddTaskInput.value === "") {
     alert("Input cannot be empty!");
     return;
   }
-
   const li = document.createElement("li");
   li.className = "collection-item";
 
-  li.appendChild(document.createTextNode(taskInput.value));
+  li.appendChild(document.createTextNode(AddTaskInput.value));
 
   const link = document.createElement("a");
   link.className = "delete-item secondary-content";
@@ -25,6 +23,22 @@ function AddTask() {
 
   li.appendChild(link);
 
-  taskCollection.appendChild(li);
-  taskInput.value = "";
-}
+  taskList.appendChild(li);
+  AddTaskInput.value = "";
+});
+
+// Remove Task
+taskList.addEventListener("click", (e) => {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+});
+
+// Remove All Tasks
+clearTasksBtn.addEventListener("click", () => {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+});
